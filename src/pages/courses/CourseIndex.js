0bbb47b74bf32aa5816f77dd111a43ec7bb8bf69
mishalp <mishalclever@gmail.com>
2023-03-02 09:36:07 +0530

    bug fix

--- src/pages/courses/CourseIndex.js
+++ src/pages/courses/CourseIndex.js
@@ -1,15 +1,61 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import SideMenu from './sideMenu/SideMenu'
-import './coursesIndex.css'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import SideMenu from "./sideMenu/SideMenu";
+import "./coursesIndex.css";
+import courses from "../../images/courses.jpg";
+
+export const recomended = [
+  {
+    name: "Video Title",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    rating: "4.4",
+    thumbnail: courses,
+  },
+  {
+    name: "Video Title",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    rating: "4.4",
+    thumbnail: courses,
+  },
+  {
+    name: "Video Title",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    rating: "4.4",
+    thumbnail: courses,
+  },
+  {
+    name: "Video Title",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    rating: "4.4",
+    thumbnail: courses,
+  },
+  {
+    name: "Video Title",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    rating: "4.4",
+    thumbnail: courses,
+  },
+  {
+    name: "Video Title",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    rating: "4.4",
+    thumbnail: courses,
+  },
+];
 
 function CoursesIndex() {
   return (
-    <div className='courses-index'>
+    <div className="courses-index">
       <SideMenu />
       <Outlet />
     </div>
-  )
+  );
 }
 
-export default CoursesIndex
+export default CoursesIndex;
--- src/pages/courses/coursesHome/CoursesHome.js
+++ src/pages/courses/coursesHome/CoursesHome.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./coursesHome.css";
 import courses from "../../../images/courses.jpg";
 import search from "../../../images/search.png";
+import { recomended } from "../CourseIndex";
 
 
 function CoursesHome() {
@@ -19,6 +20,24 @@ function CoursesHome() {
         </div>
         <div className="right">
           <img src={courses} alt="" />
+        </div>
+      </div>
+      <div className="main-div">
+        <h2>Recommended for You</h2>
+        <div className="recommended-div">
+          {recomended.map((item, index)=>(
+            <div className="card">
+              <img src={item.thumbnail} alt="" />
+              <div className="body">
+                <h4>{item.name}</h4>
+                <p>{item.description}</p>
+                <div>
+                  <p>Free plan</p>
+                  <h5>Rating:{item.rating}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
